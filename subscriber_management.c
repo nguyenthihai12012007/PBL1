b251d38c76688566082b48fe14d8bc95c860077f
trannguyennhuhoa <trannguyennhuhoa102@gmail.com>
2026-05-18 23:09:58 +0700

    update

--- subscriber_management.c
+++ subscriber_management.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <unistd.h>
 
+#include "function.h"
+#include "menu.h"
 #include "print.h"
 #include "record.h"
 #include "constants.h"
@@ -32,7 +35,6 @@ AccountNode *createAccountNode(Account A) {
     return newNode;
 }
 
-
 void toLowerCase(char *str) {
     for (int i = 0; str[i]; i++) {
         str[i] = tolower(str[i]);
@@ -48,6 +50,8 @@ int validatePhone(char phone[]) {
         if (phone[i] < '0' || phone[i] > '9') 
         return 0;
     }
+    if (phone[0] != '0')
+        return 0;
     return 1;
 }
 
@@ -299,7 +303,7 @@ Account inputAccount() {
     scanf("%s", A.password);
     getchar();
 
-    printf("Chon chuc vu : 1. Nhan vien      2. Admin");
+    printf("Chon chuc vu : 1. Admin      2. Nhan vien");
     int role;
     scanf("%d",&role);
     if(role == ROLE_ADMIN) A.role = role;
@@ -392,7 +396,6 @@ Record inputRecord() {
     return R;
 }
 
-
 Node* search_record(Node* head,char phone[15]) {
     Node* cur = head;
 
@@ -405,68 +408,6 @@ Node* search_record(Node* head,char phone[15]) {
     }
     printf("Khong tim thay thue bao!\n");
     return NULL;
-}
-
-void menu_update(Node* head) {
-    int choice;
-
-    do {
-        printf("\n1. Sua ten\n");
-        printf("2. Sua dia chi\n");
-        printf("3. Sua so dien thoai\n");
-        printf("4. Sua trang thai hoat dong\n");
-        printf("0. Thoat\n");
-        printf("Chon: ");
-        scanf("%d", &choice);
-        getchar();
-
-        switch (choice) {
-            case 1:
-                printf("Nhap ten moi: ");
-                fgets(head->data.name, sizeof(head->data.name), stdin);
-                head->data.name[strcspn(head->data.name, "\n")] = '\0';
-                printf("Sua ten thanh cong!\n");
-                pauseScreen();
-                break;
-
-            case 2:
-                printf("Nhap dia chi moi: ");
-                fgets(head->data.address,sizeof(head->data.address),stdin);
-                head->data.address[strcspn(head->data.address,"\n")]='\0';
-                printf("Sua dia chi thanh cong!\n");
-                pauseScreen();
-                break;
-
-            case 3:
-                printf("Nhap so moi: ");
-                do {
-                    scanf("%s", head->data.phone);
-                    if (!validatePhone(head->data.phone)) {
-                        printf("So dien thoai khong hop le! Nhap lai: ");
-                    }
-                } while (!validatePhone(head->data.phone));
-                getchar();
-                printf("Sua so thue bao thanh cong!\n");
-                pauseScreen();
-                break;
-
-            case 4: 
-                printf("Nhap trang thai moi: ");
-                head->data.status = inputStatus();
-                printf("Sua trang thai thanh cong!\n");
-                pauseScreen();
-                break;
-
-            case 0:
-                printf("Thoat!\n");
-                break;
-
-            default:
-                printf("Lua chon khong hop le!\n");
-                pauseScreen();
-        }
-
-    } while (choice != 0);
 }
 
 void updateAccount(AccountNode *head) {
@@ -484,7 +425,7 @@ void updateAccount(AccountNode *head) {
     }
 }
 
-void updateRecord(Node* head){
+void updateRecord(Node* head) {
     if(head == NULL) {
         printf("Danh sach rong.\n");
         return ;
@@ -1038,311 +979,15 @@ void printTop3ProvinceByFee(Node *head) {
     printTop3ProvinceFee(stats,count);
 }
 
-void statisticsMenu(Node *head) {
-    int choice;
-    do {
-        printf("\n");
-        printf(CYAN BOLD);
-        printf("+===========================================+\n");
-        printf("|               MENU THONG KE               |\n");
-        printf("+===========================================+\n");
-        printf(RESET);
-        printf("| " YELLOW "1." RESET " Thong ke so thue bao theo tinh         |\n");
-        printf("| " YELLOW "2." RESET " Tinh nhieu thue bao nhat               |\n");
-        printf("| " YELLOW "3." RESET " Xuat bao cao thong ke ra file          |\n");
-        printf("| " RED "0." RESET " Quay lai                               |\n");
-        printf(CYAN);
-        printf("+===========================================+\n");
-        printf(RESET);
-        printf(GREEN "Nhap lua chon cua ban: " RESET); 
-
-        while (1) {                       
-            if(scanf("%d", &choice) != 1) {  
-                while(getchar() != '\n');                 
-                printf(RED "Loi: Vui long nhap so! Nhap lai: " RESET);                             
-                continue;            
-            }            
-            if(choice < 0 || choice > 5) {                
-                printf(RED "Lua chon khong hop le! Nhap lai: " RESET);                
-                continue;            
-            }            
-            break;        
-        }        
-        
-        switch(choice) {
-            case 1:
-                statisticsByProvince(head);
-                break;
-            
-            case 2: 
-                pronvinceMaxRecord(head);
-                break;
-            case 3:
-                exportReport(head);
-                break;
-                
-            case 0:
-                printf("Quay lai menu chinh...\n");
-                break;
-            
-            default:
-                printf("Lua chon khong hop le!");
-        }
-
-        if (choice != 0) 
-            endScreen();
-
-    } while(choice != 0);
-}
-
-void menu_manage(Node *head) {
-    int choice;
-    do {
-        printf(CYAN BOLD);
-        printf("=======================================================\n");
-        printf("               Quan ly thue bao\n");
-        printf("=======================================================\n");
-        printf(RESET);
-        printf(YELLOW);
-        printf("| 1. %-45s |\n", "Them thue bao");
-        printf("| 2. %-45s |\n", "Sua thue bao");
-        printf("| 3. %-45s |\n", "Xoa thue bao");
-        printf("| 4. %-45s |\n", "Doc file du lieu");
-        printf("| 0. %-45s |\n", "Thoat");
-        printf(RESET);
-        printf("=======================================================\n");
-        printf(GREEN BOLD);
-        printf("Nhap lua chon cua ban: ");
-        printf(RESET);
-
-        while (1) {                       
-            if(scanf("%d", &choice) != 1) {  
-                while(getchar() != '\n');                 
-                printf(RED "Loi: Vui long nhap so! Nhap lai: " RESET);                             
-                continue;            
-            }            
-            if(choice < 0 || choice > 4) {                
-                printf(RED "Lua chon khong hop le! Nhap lai: " RESET);                
-                continue;            
-            }            
-            break;        
-        } 
-
-        switch(choice) {
-            case 1: {
-                Record newR = inputRecord();
-                addRecord(&head,newR);
-                saveToFile("data1.txt", head);
-                printf("Da them thue bao va luu vao file thanh cong!\n");
-                break;
-            }
-            case 2: 
-                updateRecord(head);
-                break;
-            case 3:
-                deleteRecord(&head);
-                break;
-            case 4:
-                readFileByUser(&head);
-                break;
-            case 0:
-                printf("Quay lai menu chinh...\n");
-                break;
-            
-            default:
-                printf("Lua chon khong hop le!");
-        }
-
-        if (choice != 0) 
-            endScreen();
-
-    } while(choice != 0);
-}
-
-void menu_search(Node *head) {
-    int choice;
-    do {
-        printf(CYAN BOLD);
-        printf("=======================================================\n");
-        printf("               Tra cuu thue bao\n");
-        printf("=======================================================\n");
-        printf(RESET);
-        printf(YELLOW);
-        printf("| 1. %-45s |\n", "Tim kiem thue bao");
-        printf("| 2. %-45s |\n", "Loc thue bao theo tinh thanh");
-        printf("| 3. %-45s |\n", "Loc thue bao theo trang thai");
-        printf("| 4. %-45s |\n", "Kiem tra trung thue bao");
-        printf("| 0. %-45s |\n", "Thoat");
-        printf(RESET);
-        printf("=======================================================\n");
-        printf(GREEN BOLD);
-        printf("Nhap lua chon cua ban: ");
-        printf(RESET);
-
-        while (1) {                       
-            if(scanf("%d", &choice) != 1) {  
-                while(getchar() != '\n');                 
-                printf(RED "Loi: Vui long nhap so! Nhap lai: " RESET);                             
-                continue;            
-            }            
-            if(choice < 0 || choice > 4) {                
-                printf(RED "Lua chon khong hop le! Nhap lai: " RESET);                
-                continue;            
-            }            
-            break;        
-        } 
-
-        switch(choice) {
-            case 1: {
-                char phone[15];
-                printf("Nhap so dien thoai thue bao can tim: ");
-                scanf("%s",phone);
-                Node* cur=search_record(head,phone);
-                if (cur != NULL) 
-                    print_record(cur);
-                endScreen();
-                break;
-            }
-            case 2: 
-                listByProvince(head);
-                break;
-            case 3:
-                filterByStatus(head);
-                break;
-            case 4:
-                checkDuplicate(&head);
-                break;
-            case 0:
-                printf("Quay lai menu chinh...\n");
-                break;
-            
-            default:
-                printf("Lua chon khong hop le!");
-        }
-
-        if (choice != 0) 
-            endScreen();
-
-    } while(choice != 0);
-}
-
-void menu_statistical(Node *head) {
-    int choice;
-    do {
-        printf(CYAN BOLD);
-        printf("=======================================================\n");
-        printf("               Thong Ke & Cuoc\n");
-        printf("=======================================================\n");
-        printf(RESET);
-        printf(YELLOW);
-        printf("| 1. %-45s |\n", "Thong ke thue bao");
-        printf("| 2. %-45s |\n", "Thong ke doanh thu"); // viết thêm hàm thống kê doanh thu
-        printf("| 3. %-45s |\n", "Tinh cuoc thue bao");
-        printf("| 0. %-45s |\n", "Thoat");
-        printf(RESET);
-        printf("=======================================================\n");
-        printf(GREEN BOLD);
-        printf("Nhap lua chon cua ban: ");
-        printf(RESET);
-
-        while (1) {                       
-            if(scanf("%d", &choice) != 1) {  
-                while(getchar() != '\n');                 
-                printf(RED "Loi: Vui long nhap so! Nhap lai: " RESET);                             
-                continue;            
-            }            
-            if(choice < 0 || choice > 4) {                
-                printf(RED "Lua chon khong hop le! Nhap lai: " RESET);                
-                continue;            
-            }            
-            break;        
-        } 
-
-        switch(choice) {
-            case 1: {
-                statisticsMenu(head);
-                break;
-            }
-            case 2: 
-                //thong ke doanh thu
-                break;
-            case 3:
-                calculateFee(head);
-                break;
-            case 0:
-                printf("Quay lai menu chinh...\n");
-                break;
-            
-            default:
-                printf("Lua chon khong hop le!");
-        }
-        if (choice != 0) 
-            endScreen();
-    } while(1);
-}
-
-void menu_account(AccountNode **head) {
-    int choice;
-    do{
-        printf(CYAN BOLD);
-        printf("=======================================================\n");
-        printf("               Quan Ly Tai Khoan\n");
-        printf("=======================================================\n");
-        printf(RESET);
-        printf(YELLOW);
-        printf("| 1. %-45s |\n", "Them nhan vien");
-        printf("| 2. %-45s |\n", "Xoa nhan vien");
-        printf("| 3. %-45s |\n", "Sua nhan vien");
-        printf("| 0. %-45s |\n", "Thoat");
-        printf(RESET);
-        printf("=======================================================\n");
-        printf(GREEN BOLD);
-        printf("Nhap lua chon cua ban: ");
-        printf(RESET);
-
-        while (1) {                       
-            if(scanf("%d", &choice) != 1) {  
-                while(getchar() != '\n');                 
-                printf(RED "Loi: Vui long nhap so! Nhap lai: " RESET);                             
-                continue;            
-            }            
-            if(choice < 0 || choice > 3) {                
-                printf(RED "Lua chon khong hop le! Nhap lai: " RESET);                
-                continue;            
-            }            
-            break;        
-        } 
-
-        switch(choice) {
-            case 1: 
-                addAccount(head);
-                endScreen();
-                break;
-            case 2: 
-                deleteAccount(head);
-                break;
-            case 3:
-                updateAccount(*head);
-                break;
-            case 0:
-                printf("Quay lai menu chinh...\n");
-                break;
-            
-            default:
-                printf("Lua chon khong hop le!");
-        }
-        if (choice != 0) 
-            endScreen();
-
-    } while(1);
-}
-
 int main() {
     Node* head = NULL;
     AccountNode *headAccount = NULL;
     Account accounts[100];
     int accountCount = 0;
     int role;
+
+    system("clear");
+    showScreen();
 
     readFile("data1.txt", &head);
     loadAccounts(accounts, &accountCount);
@@ -1362,28 +1007,24 @@ int main() {
 
             switch(choice) {
                 case 1 : 
-                    menu_manage(head);
-                    endScreen();
+                    menu_manage(&head);
                     break;
                 case 2 :
                     menu_search(head);
-                    endScreen();
                     break;
                 case 3 :
                     menu_statistical(head);
-                    endScreen();
                     break;
                 case 4 :
                     menu_account(&headAccount);
-                    endScreen();
                     break;
                 case 0 :
-                    printf("Da dang xuat\n");
+                    printf("Da dang xuat!\n");
                     break;
                 default :
-                    printf("Lua chon khong hop le");
+                    printf("Lua chon khong hop le!");
             }
-        }while(1);
+        }while(choice != 0);
     } 
     
     else if (role == ROLE_STAFF) {
@@ -1421,12 +1062,12 @@ int main() {
                     endScreen();
                     break;
                 case 0 :
-                    printf("Da dang xuat\n");
+                    printf("Da dang xuat!\n");
                     break;
                 default :
-                    printf("Lua chon khong hop le");
+                    printf("Lua chon khong hop le!");
             }
-        }while(1);
+        }while(choice != 0);
     }
     return 0;
 }
